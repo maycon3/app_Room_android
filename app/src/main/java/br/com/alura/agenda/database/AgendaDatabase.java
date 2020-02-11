9507683c4a0255c6d5b98d1ca559d9c841c22fd0
maycon <maycon.trb@gmail.com>
2020-02-10 22:04:34 -0300

    Finalizando o CRUD

---
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda.database;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import br.com.alura.agenda.database.dao.AlunoDao;
+import br.com.alura.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 2,exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+
+    public abstract AlunoDao getRoomAlunoDao();
+
+    public static AgendaDatabase getInstance(Context context) {
+        return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS).allowMainThreadQueries().build();
+    }
+}
